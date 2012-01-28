8a332d4f46e780ca591cb43bbc335cd6007b3402
Ian Smith <iansmith@acm.org>
2012-01-28 13:38:45 +0100

    updated test to reflect the fact that RSC said we can't change the json interface

--- webapp_core_test.go
+++ webapp_core_test.go
@@ -1,7 +1,6 @@
 package seven5
 
 import (
-	"betterjson"
 	"encoding/json"
 	"launchpad.net/gocheck"
 	"strings"
@@ -57,9 +56,21 @@ func (self *JSONSuite) TestUnMarshalOfPrivateString(c *gocheck.C) {
 	c.Check(result.A, gocheck.Equals, "frip")
 }
 
+//
+// http://code.google.com/p/go/issues/detail?id=2761
+//
+//
+//It's too late to be changing the json interface right now.
+//Omitempty has a clear definition.  Marshalers do not
+//get to change that definition.
+//
+//I would suggest updating the documentation but I don't
+//think there's anything to say.
+//
+//Russ
 func (self *JSONSuite) xTestMarshalOfJsonOptions(c *gocheck.C) {
 	example2 := &ex2{A: "fleazil", B: "grix"}
-	jsonContent, err := betterjson.Marshal(&example2)
+	jsonContent, err := json.Marshal(&example2)
 	result:=string(jsonContent)
 	
 	c.Assert(err, gocheck.Equals, nil)
