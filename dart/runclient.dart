9c21c4113804254cc9708a3a06643cda0a5ac217
Ian Smith <iansmith@captricity.com>
2012-10-09 14:48:31 -0700

    removed modena specific code for this package

--- dart/runclient.dart
+++
@@ -1,8 +0,0 @@
-#import("dart:json");
-#import("dart:html");
-#import("modena")
-
-main() {
-	String url = "http://localhost:3003/quote/";
-  HttpRequest request = new HttpRequest.get(url, onSuccess);
-}
