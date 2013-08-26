0a7b3c1b0aa69c037ae4735c734a075984753842
Ian Smith <iansmith@captricity.com>
2013-08-25 19:43:42 -0700

    Added basic QBS tests. Fixed migrator to compile.

--- go/src/nullblog/nullblog.go
+++ go/src/nullblog/nullblog.go
@@ -2,7 +2,9 @@
 package nullblog
 
 import (
-	_ "github.com/coocood/qbs"
+	"database/sql"
+	"fmt"
+	"github.com/iansmith/qbs"
 	"github.com/seven5/seven5"
 	"net/http"
 	"time"
@@ -12,7 +14,7 @@ import (
 //gives the content to be sent back and forth.
 type ArticleWire struct {
 	Id      seven5.Id
-	Content seven5.String255
+	Content seven5.Textblob
 	Author  seven5.String255
 }
 
@@ -29,27 +31,58 @@ type Article struct {
 	Updated time.Time
 }
 
+//NullBlogMigrate is to allow us to automatically move the database into
+//the right states, particularly for testing.
+type Migrate struct {
+}
+
+func (self *Migrate) Migrate_0001(m seven5.Migrator) error {
+	return m.CreateTableIfNotExists(&Article{})
+}
+
+func (self *Migrate) Migrate_0001_Rollback(m seven5.Migrator) error {
+	return m.DropTableIfExists(&Article{})
+}
+
 //ArticleResourceis the _implementation_ of the server side that exchanges ArticleWire with the
 //client side.
 type ArticleResource struct {
 	//STATELESS! PUTTING STATE IN A REST RESOURCE CAN CAUSE THE SUN TO BURN OUT.
 }
 
-func (IGNORED *ArticleResource) Index(bundle seven5.PBundle) (interface{}, error) {
-	return nil, nil
+func (self *Article) ToWire() *ArticleWire {
+	return &ArticleWire{
+		Id:      seven5.Id(self.Id),
+		Content: seven5.Textblob(self.Content),
+		Author:  seven5.String255(self.Author),
+	}
 }
 
-func (IGNORED *ArticleResource) Find(Id seven5.Id, bundle seven5.PBundle) (interface{}, error) {
+func (IGNORED *ArticleResource) IndexQbs(bundle seven5.PBundle, q *qbs.Qbs) (interface{}, error) {
+	var articles []*Article
+	if err := q.FindAll(&articles); err != nil {
+		return nil, err
+	}
+	result := []*ArticleWire{}
+	for _, a := range articles {
+		result = append(result, a.ToWire())
+	}
+	return result, nil
+}
+
+func (IGNORED *ArticleResource) FindQbs(Id seven5.Id, bundle seven5.PBundle, q *qbs.Qbs) (interface{}, error) {
 	i := int64(Id)
 	if i < 0 {
 		return nil, seven5.HTTPError(http.StatusBadRequest, "nice try, loser")
 	}
-	//err := hd.Where("color", "=", "green").OrderBy("name").Limit(1).Find(&results)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	return nil, nil
+	result := &Article{Id: i}
+	if err := q.WhereEqual("Id", i).Find(result); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, seven5.HTTPError(http.StatusBadRequest, fmt.Sprintf("bad id %d", i))
+		}
+		return nil, err
+	}
+	return result.ToWire(), nil
 }
 
 func (IGNORED *ArticleResource) Allow(IGNORED2 seven5.Id, IGNORED3 string, IGNORED4 seven5.PBundle) bool {
--- go/src/nullblog/nullblog_test.go
+++ go/src/nullblog/nullblog_test.go
@@ -1,49 +1,113 @@
 package nullblog
 
 import (
+	"bytes"
+	_ "github.com/lib/pq"
+	"github.com/seven5/seven5"
 	"net/http"
 	"testing"
-	"github.com/seven5/seven5"
 )
 
-//trivial test of the Index method
-func TestIndex(T *testing.T) {
-	underTest := new(ArticleResource)
-
-	result, err := underTest.Index(nil)
+func checkIndexLength(T *testing.T, underTest seven5.RestIndex, expected int) []*ArticleWire {
+	raw, err := underTest.Index(nil)
 	if err != nil {
 		T.Fatalf("unexpected error in Index: %s", err)
 	}
-	articles := result.([]*ArticleWire)
-	if len(articles) != 2 {
-		T.Fatalf("unexpected size of articles returned from Index: %d", len(articles))
+	result := raw.([]*ArticleWire)
+	if len(result) != expected {
+		T.Errorf("unexpected number of results, expected %d but got %d", expected, len(result))
 	}
+	return result
 }
 
-func TestFind(T *testing.T) {
+func TestResourceIndexQbs(T *testing.T) {
 
-	underTest := new(ArticleResource)
+	store := seven5.NewLocalhostEnvironment("nullblog", true /*test*/).GetQbsStore()
+	underTest := seven5.QbsWrapIndex(&ArticleResource{}, store)
 
-	result, err := underTest.Find(0, nil)
-	if err != nil {
-		T.Fatalf("unexpected error in Find: %s", err)
-	}
-	article := result.(*ArticleWire)
-	if article.Id != 0 || article.Author == "" || article.Content == "" {
-		T.Fatalf("unexpected content in Find %+v", article)
-	}
+	seven5.WithEmptyQbsStore(store, &Migrate{}, func() {
+		checkIndexLength(T, underTest, 0)
 
-	result, err = underTest.Find(29892, nil)
-	if result != nil {
-		T.Fatalf("expected error but got result in Find %+v", result)
-	}
-	if err == nil {
-		T.Fatalf("nothing returned from Find (two nils)!")
-	}
+		//put content in DB by hand
+		a := &Article{}
+		a.Author = "Ian Smith"
+		var b bytes.Buffer
+		for i := 0; i < 1000; i++ {
+			b.WriteString("this is a test.")
+		}
+		a.Content = b.String()
 
-	http_error := err.(*seven5.Error)
-	if http_error.StatusCode != http.StatusBadRequest {
-		T.Errorf("wrong error code, expect BadRequest but got %d", http_error.StatusCode)
-	}
+		if _, err := store.Q.Save(a); err != nil {
+			T.Fatalf("unexpected error saving content (%s)", err)
+		}
+
+		//test that content is returned
+		checkIndexLength(T, underTest, 1)
+
+		//add more content
+		a = &Article{}
+		a.Author = "Joe Blow"
+		a.Content = "Bad Web Frameworks... blah blah blah"
+
+		if _, err := store.Q.Save(a); err != nil {
+			T.Fatalf("unexpected error saving content (%s)", err)
+		}
+
+		//test that content is returned
+		checkIndexLength(T, underTest, 2)
+
+	})
+}
+
+func TestResourceFindQbs(T *testing.T) {
+
+	store := seven5.NewLocalhostEnvironment("nullblog", true /*test*/).GetQbsStore()
+	underTest := seven5.QbsWrapFind(&ArticleResource{}, store)
+
+	seven5.WithEmptyQbsStore(store, &Migrate{}, func() {
+		//NOTE: no connection to the database here! this is the code under test!
+		result, err := underTest.Find(-22, nil)
+		if err == nil {
+			T.Fatalf("expected error but didn't receive one from Find!")
+		}
+
+		http_error := err.(*seven5.Error)
+		if http_error.StatusCode != http.StatusBadRequest {
+			T.Errorf("wrong error code, expect BadRequest but got %d", http_error.StatusCode)
+		}
+
+		//write some content in the db
+		a := &Article{}
+		ian := "Ian Smith"
+		shirt := "a bit shirty"
+		a.Author = ian
+		a.Content = shirt
+
+		result_id, err := store.Q.Save(a)
+		if err != nil {
+			T.Fatalf("unexpected error in Save (%s)", err)
+		}
+
+		//NOTE: no connection to the database here! this is the code under test!
+		result, err = underTest.Find(seven5.Id(result_id), nil)
+		if err != nil {
+			T.Fatalf("unexpected error in Find (%s)", err)
+		}
+		wire := result.(*ArticleWire)
+		if string(wire.Author) != ian || string(wire.Content) != shirt {
+			T.Errorf("wrong content received from Find (%+v)")
+		}
+
+		result, err = underTest.Find(seven5.Id(result_id-1), nil)
+		if err == nil {
+			T.Fatalf("expected error but didn't get it from Find")
+		}
+
+		http_error = err.(*seven5.Error)
+		if http_error.StatusCode != http.StatusBadRequest {
+			T.Errorf("wrong error code, expect BadRequest but got %d", http_error.StatusCode)
+		}
+
+	})
 
 }
